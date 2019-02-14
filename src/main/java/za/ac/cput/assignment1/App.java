a81ea8ff12e9b923440341c3188d3ded02679f66
MuhammadAmoo <muhammadamoo@gmail.com>
2019-02-13 22:10:31 -0800

    Updated code to ask for first name

--- src/main/java/za/ac/cput/assignment1/App.java
+++ src/main/java/za/ac/cput/assignment1/App.java
@@ -1,5 +1,6 @@
 package za.ac.cput.assignment1;
 
+import java.util.Scanner;
 /**
  * Hello world!
  *
@@ -9,5 +10,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Barebone" );
+
+        Scanner user_input = new Scanner(System.in);
+
+        String first_name;
+        System.out.println( "Whats your first name Barebone" );
+        first_name = user_input.next();
+
+        System.out.println( "Nice to meet you " + first_name + " Barebone" );
+
     }
+
+
+
 }
